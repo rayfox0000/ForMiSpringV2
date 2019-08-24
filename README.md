998efd22db85205acbe9d1ad28052b8c01bc712e
rayfox0000 <13021449@imail.sunway.edu.my>
2019-08-25 01:54:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ForMiSpringV2
---
+++ src/main/java/com/project/ForMiSpringV2/ForMiSpring/ForMiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.ForMiSpringV2.ForMiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMiSpringApplication.class, args);
+	}
+
+}
